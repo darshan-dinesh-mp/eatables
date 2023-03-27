8f7ef97f6fcb278281812d55cdee063aafcee423
devil-striker <darshaninsta360@gmail.com>
2023-03-27 21:02:07 +0530

    css

--- login.php
+++ login.php
@@ -18,37 +18,6 @@
     }
     if (!isset($_POST['submit'])) {
         ?>
-        <!-- <div className="bg-brand bg-img min-h-screen flex flex-col items-center py-4 px-4 md:px-16">
-            <div className="flex items-center w-full justify-between md:pt-4">
-                <div className="flex items-center cursor-default">
-                    <h2 className="pl-1 md:pl-2 font-poppy font-medium text-sm md:text-lg">Login to your account</h2>
-                </div>
-            </div>
-            <div className="flex items-center justify-center flex-col h-[90vh] my-12 md:mt-0">
-                <div className="flex flex-col items-center justify-center pb-2 md:pb-4">
-                    <h1 className="outline-none text-5xl md:text-6xl font-colvet cursor-none">eatables.</h1>
-                    <p className="font-poppy text-sm md:text-md">Find your next favorite.</p>
-                </div>
-                <form action="login.php" method="POST"
-                    className="grid md:grid-cols-2 md:grid-rows-2 grid-cols-1 gap-3 mt-4 md:mt-0 mb-8 place-items-center">
-                    <input type="text" name="username" placeholder="username"
-                        className="border-none w-full outline-none text-xl md:text-2xl px-6 py-3 placeholder:opacity-70 text-center placeholder:font-poppy bg-off-brand placeholder:text-dense font-poppy hover:placeholder:-translate-y-20 placeholder:duration-[0.5s]"
-                        autoComplete="off" />
-                    <input type="password" name="password" placeholder="password" autoComplete="off"
-                        className="border-none w-full outline-none text-xl md:text-2xl px-6 py-3 placeholder:opacity-70 text-center placeholder:font-poppy bg-off-brand placeholder:text-dense font-poppy hover:placeholder:-translate-y-20 placeholder:duration-[0.5s]" />
-                    <input type="submit" name="submit" value="Login"
-                        className="py-[0.50rem] md:py-[0.70rem] bg-dense w-44 md:col-span-2 text-white px-9 hover:cursor-pointer text-xl font-poppy rounded-md hover:bg-dense duration-500">
-                    </input>
-                </form>
-                <p className="text-sm text-center md:text-lg font-poppy">
-                    New to eatables?
-                    <a href="register.php">
-                        <span className="underline pl-1 cursor-pointer">Sign up</span>
-                    </a>
-                    .
-                </p>
-            </div>
-        </div> -->
         <div class="bg-brand bg-img min-h-screen grid">
         <div class="flex flex-col items-center justify-center">
             <div class="flex flex-col items-center justify-center py-16 md:py-36 md:space-y-4 space-y-2">
@@ -71,10 +40,10 @@
                 </div>
             </div>
             <div class="md:space-x-4 flex flex-col md:flex-row items-center justify-center">
-                <a href="/forgotpassword" as="/forgotpassword" class="font-poppy md:text-lg underline hover:text-gray-700">
+                <a href="forgotpassword.php" as="/forgotpassword" class="font-poppy md:text-lg underline hover:text-gray-700">
                     forgot password?
                 </a>
-                <a href="/signup" as="/signup" class="font-poppy md:text-lg underline hover:text-slate-800">
+                <a href="signup.php" as="/signup" class="font-poppy md:text-lg underline hover:text-slate-800">
                     create account
                 </a>
             </div>
--- signup.php
+++ signup.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>Eatables|Register</title>
 
 </head>
@@ -17,40 +18,46 @@
     }
     if (!isset($_POST['submit'])) {
         ?>
-        <div className="bg-brand bg-img min-h-screen flex flex-col items-center py-4 px-4 md:px-16">
-            <div className="flex items-center w-full justify-between md:pt-4">
-                <div className="flex items-center cursor-default">
-                    <h2 className="pl-1 md:pl-2 font-poppy font-medium text-sm md:text-lg">create account</h2>
+        <div class="bg-brand bg-img min-h-screen flex flex-col items-center py-4 px-4 md:px-16">
+            <div class="flex items-center w-full justify-between md:pt-4">
+                <div class="flex items-center">
+                    <FontAwesomeIcon class="text-sm md:text-lg" icon={faUser} />
+                    <h2 class="pl-1 md:pl-2 font-poppy font-medium text-sm md:text-lg">create account</h2>
                 </div>
             </div>
-            <div className="flex items-center justify-center flex-col h-[90vh] my-12 md:mt-0">
-                <div className="flex flex-col items-center justify-center pb-2 md:pb-4">
-                    <h1 className="outline-none text-5xl md:text-6xl font-colvet cursor-none">eatables.</h1>
-                    <p className="font-poppy text-sm md:text-md">Find your next favorite.</p>
+            <div class="flex items-center justify-center flex-col h-[90vh] my-12 md:mt-0">
+                <div class="flex flex-col items-center justify-center pb-2 md:pb-4">
+                    <a href="/" as="/" class="outline-none text-5xl md:text-6xl font-colvet">
+                        eatables.
+                    </a>
+                    <p class="font-poppy text-sm md:text-md">Find your next favorite.</p>
                 </div>
-                <form action="signup.php" method="POST"
-                    className="grid md:grid-cols-2 md:grid-rows-2 grid-cols-1 gap-3 mt-4 md:mt-0 mb-8 place-items-center">
+
+                <form action="/api/register" method="post"
+                    class="grid md:grid-cols-2 md:grid-rows-2 grid-cols-1 gap-3 mt-4 md:mt-0 mb-8 place-items-center">
                     <input type="text" name="fullname"
-                        className="border-none outline-none w-full text-xl md:text-2xl px-3 py-3  placeholder:opacity-70 text-center placeholder:font-poppy bg-off-brand placeholder:text-dense font-poppy hover:placeholder:-translate-y-20 placeholder:duration-[0.5s] md:col-span-2"
+                        class="border-none outline-none w-full text-xl md:text-2xl px-3 py-3  placeholder:opacity-70 text-center placeholder:font-poppy bg-off-brand placeholder:text-dense font-poppy hover:placeholder:-translate-y-20 placeholder:duration-[0.5s] md:col-span-2"
                         placeholder="fullname" autoComplete="off" />
-                    <input type="text" name="username" placeholder="username"
-                        className="border-none w-full outline-none text-xl md:text-2xl px-6 py-3 placeholder:opacity-70 text-center placeholder:font-poppy bg-off-brand placeholder:text-dense font-poppy hover:placeholder:-translate-y-20 placeholder:duration-[0.5s]"
-                        autoComplete="off" />
-                    <input type="email" name="email" placeholder="email"
-                        className="border-none w-full outline-none text-xl md:text-2xl px-6 py-3 placeholder:opacity-70 text-center placeholder:font-poppy bg-off-brand placeholder:text-dense font-poppy hover:placeholder:-translate-y-20 placeholder:duration-[0.5s]"
-                        autoComplete="off" />
-                    <input type="password" name="password" placeholder="password" autoComplete="off"
-                        className="border-none w-full outline-none text-xl md:text-2xl px-6 py-3 placeholder:opacity-70 text-center placeholder:font-poppy bg-off-brand placeholder:text-dense font-poppy hover:placeholder:-translate-y-20 placeholder:duration-[0.5s]" />
-                    <input type="password" name="cpassword" placeholder="confirm" autoComplete="off"
-                        className="border-none w-full outline-none text-xl md:text-2xl px-6 py-3 placeholder:opacity-70 text-center placeholder:font-poppy bg-off-brand placeholder:text-dense font-poppy hover:placeholder:-translate-y-20 placeholder:duration-[0.5s]" />
-                    <input type="submit" name="submit" value="Sign Up"
-                        className="py-[0.50rem] md:py-[0.70rem] bg-dense w-44 md:col-span-2 text-white px-9 hover:cursor-pointer text-xl font-poppy rounded-md hover:bg-dense duration-500">
-                    </input>
+                    <input type="text" name="username"
+                        class="border-none w-full outline-none text-xl md:text-2xl px-6 py-3 placeholder:opacity-70 text-center placeholder:font-poppy bg-off-brand placeholder:text-dense font-poppy hover:placeholder:-translate-y-20 placeholder:duration-[0.5s]"
+                        placeholder="username" maxLength={15} minLength={4} autoComplete="off" />
+
+                    <input type="email" name="email"
+                        class="border-none w-full outline-none text-xl md:text-2xl px-6 py-3 placeholder:opacity-70 text-center placeholder:font-poppy bg-off-brand placeholder:text-dense font-poppy hover:placeholder:-translate-y-20 placeholder:duration-[0.5s]"
+                        placeholder="email" autoComplete="off" />
+                    <input type="password" name="password"
+                        class="border-none w-full outline-none text-xl md:text-2xl px-6 py-3 placeholder:opacity-70 text-center placeholder:font-poppy bg-off-brand placeholder:text-dense font-poppy hover:placeholder:-translate-y-20 placeholder:duration-[0.5s]"
+                        placeholder="password" autoComplete="off" />
+                    <input type="password" name="confirm"
+                        class="border-none w-full outline-none text-xl md:text-2xl px-6 py-3 placeholder:opacity-70 text-center placeholder:font-poppy bg-off-brand placeholder:text-dense font-poppy hover:placeholder:-translate-y-20 placeholder:duration-[0.5s]"
+                        placeholder="confirm" autoComplete="off" />
+                    <input type="submit" value="explore"
+                        class="py-[0.50rem] md:py-[0.70rem] bg-dense w-44 md:col-span-2 text-white px-9 hover:cursor-pointer text-xl font-poppy rounded-md hover:bg-dense duration-500" />
                 </form>
-                <p className="text-sm text-center md:text-lg font-poppy">
+                <p class="text-sm text-center md:text-lg font-poppy">
                     have an account?
-                    <a href="login.php">
-                        <span className="underline pl-1 cursor-pointer">Login</span>
+                    <a href="login.php" as="/" class="underline pl-1">
+                        Log In
                     </a>
                     .
                 </p>
