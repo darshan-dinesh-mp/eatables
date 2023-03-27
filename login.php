d65b46c8c5120b1e37a57152a5e9a5bba355409d
devil-striker <darshaninsta360@gmail.com>
2023-03-27 20:47:10 +0530

    css

--- login.php
+++ login.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>Eatables|Login</title>
 
 </head>
@@ -17,7 +18,7 @@
     }
     if (!isset($_POST['submit'])) {
         ?>
-        <div className="bg-brand bg-img min-h-screen flex flex-col items-center py-4 px-4 md:px-16">
+        <!-- <div className="bg-brand bg-img min-h-screen flex flex-col items-center py-4 px-4 md:px-16">
             <div className="flex items-center w-full justify-between md:pt-4">
                 <div className="flex items-center cursor-default">
                     <h2 className="pl-1 md:pl-2 font-poppy font-medium text-sm md:text-lg">Login to your account</h2>
@@ -47,7 +48,38 @@
                     .
                 </p>
             </div>
+        </div> -->
+        <div class="bg-brand bg-img min-h-screen grid">
+        <div class="flex flex-col items-center justify-center">
+            <div class="flex flex-col items-center justify-center py-16 md:py-36 md:space-y-4 space-y-2">
+                <div class="flex flex-col items-center pb-2 md:pb-4">
+                    <a href="/" as="/" class="text-5xl md:text-6xl font-colvet">
+                        eatables.
+                    </a>
+                    <p class="font-poppy text-sm md:text-md">Find your next favorite.</p>
+                </div>
+                <form action="/api/login" method="post" class="grid place-items-center md:grid-rows-2 grid-cols-1 gap-3 mx-4">
+                    <input class="border-dense w-full outline-none text-xl md:text-2xl px-3 py-3 md:px-24 md:py-4 placeholder:opacity-70 text-center placeholder:font-poppy bg-off-brand placeholder:text-dense font-poppy hover:placeholder:-translate-y-20 placeholder:duration-[0.5s]" placeholder="username" type="text" name="username" autoComplete="off" />
+                    <input class="border-dense w-full outline-none text-xl md:text-2xl px-3 py-3 md:px-24 md:py-4 placeholder:opacity-70 text-center placeholder:font-poppy bg-off-brand placeholder:text-dense font-poppy hover:placeholder:-translate-y-20 placeholder:duration-[0.5s]" placeholder="password" type="password" name="password" autoComplete="off" />
+                    <input class="py-[0.50rem] md:py-[0.70rem] bg-dense tracking-wider text-white px-9 md:px-12 text-xl font-poppy rounded-md hover:bg-dense duration-500 " type="submit" />
+                </form>
+                <h1 class="font-poppy text-sm">OR</h1>
+                <div class="flex items-center justify-center space-x-3">
+                    <a href="/api/auth/signin" class=" bg-dense text-white px-6 py-3 text-sm font-poppy rounded-md hover:bg-dense duration-500" type="button">
+                        other sign in options
+                    </a>
+                </div>
+            </div>
+            <div class="md:space-x-4 flex flex-col md:flex-row items-center justify-center">
+                <a href="/forgotpassword" as="/forgotpassword" class="font-poppy md:text-lg underline hover:text-gray-700">
+                    forgot password?
+                </a>
+                <a href="/signup" as="/signup" class="font-poppy md:text-lg underline hover:text-slate-800">
+                    create account
+                </a>
+            </div>
         </div>
+    </div>
         <?php
     } else {
         session_start();
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 module.exports = {
   content: ["./*.{html,js,php}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+          brand: "#F9BB21",
+          "off-brand": "rgb(255,255,255,39%)",
+          "btn-black": "rgb(93,93,93,72%)",
+          dense: "#000000",
+          white: "#E9F3F8",
+          "white-op": "rgb(255,255,255,50%)",
+          "black-op": "rgb(0,0,0,50%)",
+      },
+      backgroundImage: {
+          img: "url(../media/images/BgCover.webp)",
+      },
+      fontFamily: {
+          poppy: ["Poppins", "sans-serif"],
+          colvet: ["Coolvetica", "sans-serif"],
+      },
+  },
   },
   plugins: [],
 }
