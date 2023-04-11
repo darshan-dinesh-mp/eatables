3f0a6ba440314f9bf04291f4f6da1d9d62155586
Delbin George <imdelbingeorge@gmail.com>
2023-04-11 11:41:51 +0530

    small changes

--- index.php
+++ index.php
@@ -11,7 +11,6 @@ if (!$_SESSION['status']) {
 
 	<head>
 		<meta charset="UTF-8">
-		<!-- <meta charset="UTF-8"> -->
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<title>Eatables - Search</title>
