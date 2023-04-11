c81078c66314f69ce871d26a74bab4b63efb676d
Ashwin <ashwinkv.akv@gmail.com>
2023-04-11 11:56:06 +0530

    some changes

---
+++ logout.php
@@ -0,0 +1,11 @@
+<?php
+session_start();
+if (isset($_SESSION['status'])) {
+    if (isset($_POST['logout'])) {
+        session_destroy();
+        header("Location: login.php");
+        exit;
+    }
+}
+?>
+
