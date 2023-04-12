aa6b070dbe316fd298a4079fb3ed0daef35e7228
devil-striker <darshaninsta360@gmail.com>
2023-04-12 23:58:04 +0530

    review dynamic

--- itempage.php
+++ itempage.php
@@ -39,10 +39,6 @@
         if (mysqli_connect_error()) {
             die("Not connected");
         }
-
-        $path = "media/images/user-images/";
-        $image = $path . $_SESSION['img'];
-
         if (isset($_GET['item_id'])) {
             $item_id = $_GET['item_id'];
             $_SESSION['item_id'] = $item_id;
@@ -64,34 +60,40 @@
                             echo "<h1 class='text-3xl font-bold capitalize'>$row[hotel_name]</h1>";
                             echo "<h2 class='text-2xl font-medium'>$row[item_name]</h2>";
                             echo "<h3 class='text-2xl font-medium'><span class='text-lg'>₹</span>" . $row["item_price"] . ".00</h3>";
-                    ?>
-                </div>
-                <div class="w-full">
-                    <form method="post" class="">
-                        <input type="hidden" name="item_id">
-                        <button type="submit" class="group flex items-center space-x-2 font-poppy font-semibold bg-white/40 py-2 px-8 rounded-full" name="add_favourite">
-                            <i class="fa-solid fa-heart text-3xl text-red-600 group-hover:scale-[1.10] animate-pulse duration-500"></i>
-                            <h1>Add to favouirte</h1>
+                            ?>
+                        </div>
+                        <div class="w-full">
+                            <form method="post" class="">
+                                <input type="hidden" name="item_id">
+                                <button type="submit"
+                                    class="group flex items-center space-x-2 font-poppy font-semibold bg-white/40 py-2 px-8 rounded-full"
+                                    name="add_favourite">
+                                    <i
+                                        class="fa-solid fa-heart text-3xl text-red-600 group-hover:scale-[1.10] animate-pulse duration-500"></i>
+                                    <h1>Add to favouirte</h1>
+                                </button>
+                            </form>
+                        </div>
+                    </div>
+                    <?php
+                        }
+
+                        ?>
+                <div class="w-full flex items-start flex-col my-4">
+
+                    <form action="itempage.php" class="flex items-center justify-center" method="post">
+                        <input type='text' maxlength="150"
+                            class="hover:border-brand outline-none opacity-90 border-0 text-xl md:text-2xl px-10 py-[0.80rem] rounded-md md:px-16 placeholder:opacity-70 text-center placeholder:font-poppy bg-off-brand placeholder-color font-poppy hover:placeholder:-translate-y-20 placeholder:duration-[0.5s]"
+                            placeholder="write your review here." name="review" id="review" />
+                        <button type="submit" class="ml-3" name="submit">
+                            <i class="fa-solid fa-location-arrow text-4xl hover:rotate-[50deg] duration-500"></i>
                         </button>
                     </form>
                 </div>
+
             </div>
-        <?php
-                        }
 
-        ?> <div class="w-full flex items-start flex-col my-4">
-
-            <form action="itempage.php" class="flex items-center justify-center" method="post">
-                <input type='text' maxlength="150" class="hover:border-brand outline-none opacity-90 border-0 text-xl md:text-2xl px-10 py-[0.80rem] rounded-md md:px-16 placeholder:opacity-70 text-center placeholder:font-poppy bg-off-brand placeholder-color font-poppy hover:placeholder:-translate-y-20 placeholder:duration-[0.5s]" placeholder="write your review here." name="review" id="review" />
-                <button type="submit" class="ml-3" name="submit">
-                    <i class="fa-solid fa-location-arrow text-4xl hover:rotate-[50deg] duration-500"></i>
-                </button>
-            </form>
-        </div>
-
-        </div>
-
-    <?php
+            <?php
                     }
                     //adding to favouirte
                     $user_id = $_SESSION["id"];
@@ -141,37 +143,39 @@
                     }
 
                     // Retrieve reviews from database
-                    $sql = "SELECT review.review_content, user.uname
+                    $sql = "SELECT review.review_content, user.uname, user.img
         FROM review
         INNER JOIN user
         ON review.uid = user.uid where item_id = $item_id;
         ";
                     $result = mysqli_query($con, $sql);
-    ?>
-    <div>
-        <h1 class="text-xl font-poppy font-medium">Latest Reviews</h1>
+                    ?>
+        <div>
+            <h1 class="text-xl font-poppy font-medium">Latest Reviews</h1>
 
-        <?php
-        // Display reviews
-        if (mysqli_num_rows($result) > 0) {
-            while ($row = mysqli_fetch_assoc($result)) {
-                echo "
+            <?php
+            // Display reviews
+            if (mysqli_num_rows($result) > 0) {
+                while ($row = mysqli_fetch_assoc($result)) {
+                    $img = "media/images/user-image/" . $row["img"];
+                    echo "
+                    <div class='w-full'>
                 <div class='flex items-start py-8 bg-black/50 px-8 rounded-xl text-white mb-4'>
                     <div class='flex items-start flex-col justify-center mr-3'>
-                    <p class='w-10 h-10 rounded-full bg-black'></p>
+                    <img src=$img class='w-10 h-10 rounded-full bg-black'/p>
                 </div>  
                 <div class='flex items-start flex-col justify-center'>
-                        <h1 class='text-xl font-poppy font-medium'>Darshan Dinesh MP</h1>
-                        <p class='font-poppy text-lg pt-1'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit adipisci ex velit ad laborum, dolorum vel autem maiores cumque ea? Ratione, sit voluptas cumque harum deleniti officiis et eveniet labore?</p>
+                        <h1 class='text-xl font-poppy font-medium'>$row[uname]</h1>
+                        <p class='font-poppy text-lg pt-1'>$row[review_content]</p>
                     </div>
                 </div>
         ";
+                }
+            } else {
+                echo "No reviews found.";
             }
-        } else {
-            echo "No reviews found.";
-        }
-        ?>
-    </div>
+            ?>
+        </div>
     </div>
     </div>
 </body>
