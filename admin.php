deb23c379a54ff714745cb42e407731ac2a2c10d
Delbin George <imdelbingeorge@gmail.com>
2023-04-13 03:06:42 +0530

    some chnage

--- admin.php
+++ admin.php
@@ -1,40 +1,49 @@
+<?php
+session_start();
+if (!$_SESSION['status']) {
+	header("Location: login.php");
+	exit;
+} elseif ($_SESSION['user_type'] != 0) {
+	header("Location: login.php");
+	echo "<script>alert('engoottaaaaaa')</script>";
+}
+?>
 <!DOCTYPE html>
-<html>
+<html lang="en">
 
 <head>
-	<title>Eatables</title>
-	<link rel="stylesheet" href="style.css">
-	<!-- <link rel="stylesheet" href="input.css"> -->
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>
+		Eatables - Admin
+	</title>
+	<link rel="stylesheet" href="styles/input.css">
+	<link rel="stylesheet" href="styles/style.css">
+	<script src="https://cdn.tailwindcss.com"></script>
+	<script src="https://kit.fontawesome.com/08ae7c27bc.js" crossorigin="anonymous"></script>
+	<link rel="shortcut icon" href="public/eatables.png" type="image/x-icon">
 </head>
-<body>
-	<?php
-	session_start();
-	if (!$_SESSION['status']) {
 
-		header("Location: login.php");
-		exit;
-		
-	}
-	elseif($_SESSION['user_type'] != 0) {
-		header("Location: login.php");
-		echo"<script>alert('engoottaaaaaa')</script>";
-	}
-	?>
-	<form action="logout.php" method="post" id="logout-form">
-		<input type="submit" name="logout" value="LOGOUT">
-	</form>
-	<table>
-		<tr>
-			<th>
-				<a href="manage_restaurant.php"><b>Manage restaurants</b></a>
-			</th>
-		</tr>
-		<tr>
-			<th>
-			<b><?php echo "<a href='add_hotel.php'>ADD NEW RESTAURANT</a>"; ?></b>
-			</th>
-		</tr>
-	</table>
+<body>
+	<div class="bg-brand min-h-screen bg-img w-full flex flex-col items-center p-4 md:py-0 md:px-16">
+		<div class="flex items-center w-full justify-between md:pt-4">
+			<a href="index.php" class="text-2xl md:text-4xl font-colvet">
+				<i class="fa-solid fa-user text-2xl"></i>admin eatables.
+			</a>
+			<form action="logout.php" method="post">
+				<button type="submit" name="logout" class="logout-btn">
+					<i class="fa-solid fa-right-from-bracket text-2xl"></i>
+				</button>
+			</form>
+		</div>
+		<div class="flex flex-col items-center justify-center w-full pt-8 md:pt-12">
+
+			<a href="manage_restaurant.php">Manage restaurants</a>
+			<?php echo "<a href='add_hotel.php'>ADD NEW RESTAURANT</a>"; ?>
+
+		</div>
+	</div>
 </body>
 
 </html>
