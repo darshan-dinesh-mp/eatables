6ecec8f1ecdc750207e5d4fcc6742d1b9c16c924
Delbin George <imdelbingeorge@gmail.com>
2023-04-16 14:10:29 +0530

    added submit res

--- add-restaurant.php
+++ add-restaurant.php
@@ -24,59 +24,54 @@
                     eatables.
                </a>
           </div>
-          <div class="grid gap-3 grid-cols-1 w-full mt-16 md:mt-40 space-y-0 place-items-center font-poppy">
+          <div class="grid gap-3 grid-cols-1 w-full mt-16 md:mt-24 space-y-0 place-items-center font-poppy">
+               <div class="flex flex-col items-center justify-center pb-2 md:pb-4">
+                    <h1 class="outline-none text-xl md:text-2xl font-poppy">
+                         Found a restaurant that's not on our list?
+                    </h1>
+                    <h1 class="outline-none text-lg md:text-xl font-poppy">
+                         Fill all the details below.
+                    </h1>
+               </div>
                <div class="flex items-center justify-around relative w-full md:w-2/4">
-                    <form class="w-full max-w-lg">
-                         <div class="flex flex-wrap -mx-3 mb-6">
-                              <div class="w-full px-3 mb-6 md:mb-0">
-                                   <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
-                                        Your Name
-                                   </label>
-                                   <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" value="<?php echo $fullname;  ?>">
-                              </div>
+                    <form class="grid md:grid-cols-2 md:grid-rows-2 grid-cols-1 gap-3 mt-4 md:my-2 place-items-center">
+                         <div class="w-full md:col-span-2">
+                              <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
+                                   Your Name
+                              </label>
+                              <input class="border-none outline-none w-full text-xl md:text-2xl px-3 py-3 text-center placeholder:font-poppy bg-off-brand placeholder-color font-poppy hover:placeholder:-translate-y-20 placeholder:duration-[0.5s]" id="grid-first-name" type="text" value="<?php echo $fullname;  ?>">
                          </div>
-                         <div class="flex flex-wrap -mx-3 mb-6">
-                              <div class="w-full px-3">
-                                   <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-password">
-                                        Restaurant Name
-                                   </label>
-                                   <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-password" type="text">
-                              </div>
+                         <div class="w-full md:col-span-2">
+                              <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-password">
+                                   Restaurant Name
+                              </label>
+                              <input class="border-none outline-none w-full text-xl md:text-2xl px-3 py-3 text-center placeholder:font-poppy bg-off-brand placeholder-color font-poppy hover:placeholder:opacity-0 placeholder:duration-[0.5s] md:col-span-2" id="grid-password" type="text" placeholder="The Martian Munchies">
                          </div>
-                         <div class="flex flex-wrap -mx-3 mb-2">
-                              <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-                                   <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-city">
-                                        City
-                                   </label>
-                                   <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-city" type="text">
-                              </div>
-                              <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-                                   <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-state">
-                                        State
-                                   </label>
-                                   <div class="relative">
-                                        <select class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state">
-                                             <option>Kerala</option>
-                                             <option>Karnataka</option>
-                                             <option>Tamil Nadu</option>
-                                        </select>
-                                        <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-                                             <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                                                  <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
-                                             </svg>
-                                        </div>
-                                   </div>
-                              </div>
-                              <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-                                   <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-zip">
-                                        Zip
-                                   </label>
-                                   <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-zip" type="text">
-                              </div>
-                              <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-                                   <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-zip" type="submit">
-                              </div>
+                         <div>
+                              <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-password">
+                                   Place
+                              </label>
+                              <input class="border-none outline-none w-full text-xl md:text-2xl px-3 py-3 text-center placeholder:font-poppy bg-off-brand placeholder-color font-poppy hover:placeholder:opacity-0 placeholder:duration-[0.5s] md:col-span-2" id="grid-password" type="text" placeholder="Olympus Colony">
                          </div>
+                         <div class="w-full ">
+                              <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
+                                   Contact Info
+                              </label>
+                              <input class="border-none outline-none w-full text-xl md:text-2xl px-3 py-3 text-center placeholder:font-poppy bg-off-brand placeholder-color font-poppy hover:placeholder:opacity-0 placeholder:duration-[0.5s]" id="grid-first-name" type="text" placeholder="hotelmail@hotel.com">
+                         </div>
+                         <div>
+                              <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-password">
+                                   State
+                              </label>
+                              <input class="border-none outline-none w-full text-xl md:text-2xl px-3 py-3 text-center placeholder:font-poppy bg-off-brand placeholder-color font-poppy hover:placeholder:opacity-0 placeholder:duration-[0.5s] md:col-span-2" id="grid-password" type="text" placeholder="Mars">
+                         </div>
+                         <div>
+                              <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-password">
+                                   Zip Code
+                              </label>
+                              <input class="border-none outline-none w-full text-xl md:text-2xl px-3 py-3 text-center placeholder:font-poppy bg-off-brand placeholder-color font-poppy hover:placeholder:opacity-0 placeholder:duration-[0.5s] md:col-span-2" id="grid-password" type="text" placeholder="575002">
+                         </div>
+                         <input class="py-[0.50rem] md:py-[0.70rem] w-44 md:col-span-2 text-white px-9 hover:cursor-pointer text-xl font-poppy rounded-md hover: duration-500" type="submit" />
                     </form>
                </div>
           </div>
