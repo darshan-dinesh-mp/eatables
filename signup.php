3daeccb143a7daa222c4e3be31e2d380a3890bd0
Ashwin <ashwinkv.akv@gmail.com>
2023-04-17 23:29:35 +0530

    corrected session error

--- signup.php
+++ signup.php
@@ -13,6 +13,68 @@
     <script src="https://cdn.tailwindcss.com?plugins=forms,typography,aspect-ratio,line-clamp"></script> -->
     <title>Eatables - Register</title>
 
+<script>
+    function validateFullname() {
+        const fullnameInput = document.getElementById('fullname');
+        const errorFullname = document.getElementById('error-fullname');
+
+        if (fullnameInput.value === '') {
+            errorFullname.textContent = 'Please enter your fullname';
+        } else if (!/^[A-Za-z\s]+$/.test(fullnameInput.value)) {
+            errorFullname.textContent = 'Fullname can only contain letters and spaces';
+        } else {
+            errorFullname.textContent = '';
+        }
+    }
+    function validateUsername() {
+        const usernameInput = document.getElementById('fullname');
+        const errorUsername = document.getElementById('error-fullname');
+
+        if (fullnameInput.value === '') {
+            errorUsername.textContent = 'Please enter the Username';
+        } else if (!ctype_alpha($username)) {
+            errorUsername.textContent = 'Username can only contain alphabets';
+        } else {
+            errorUsername.textContent = '';
+        }
+    }
+    function validateEmail() {
+        const fullnameInput = document.getElementById('fullname');
+        const errorEmail = document.getElementById('error-fullname');
+
+        if (fullnameInput.value === '') {
+            errorEmail.textContent = 'Please enter your fullname';
+        } else if (!/^[A-Za-z\s]+$/.test(fullnameInput.value)) {
+            errorEmail.textContent = 'Fullname can only contain letters and spaces';
+        } else {
+            errorEmail.textContent = '';
+        }
+    }
+    function validatePassword() {
+        const fullnameInput = document.getElementById('fullname');
+        const errorPass = document.getElementById('error-fullname');
+
+        if (fullnameInput.value === '') {
+            errorPass.textContent = 'Please enter your fullname';
+        } else if (!/^[A-Za-z\s]+$/.test(fullnameInput.value)) {
+            errorPass.textContent = 'Fullname can only contain letters and spaces';
+        } else {
+            errorPass.textContent = '';
+        }
+    }
+    function validateConfirm() {
+        const fullnameInput = document.getElementById('fullname');
+        const errorconf = document.getElementById('error-fullname');
+
+        if (fullnameInput.value === '') {
+            errorconf.textContent = 'Please enter your fullname';
+        } else if (!/^[A-Za-z\s]+$/.test(fullnameInput.value)) {
+            errorconf.textContent = 'Fullname can only contain letters and spaces';
+        } else {
+            errorconf.textContent = '';
+        }
+    }
+</script>
 </head>
 
 <body>
@@ -35,11 +97,16 @@
                 <p class="font-poppy text-sm md:text-md">Find your next favourite.</p>
             </div>
             <form action="signup.php" method="post" class="grid md:grid-cols-2 md:grid-rows-2 grid-cols-1 gap-3 mt-4 md:my-2 place-items-center">
-                <input type="text" name="fullname" id="fullname" class="border-none outline-none w-full text-xl md:text-2xl px-3 py-3 text-center placeholder:font-poppy bg-off-brand placeholder-color font-poppy hover:placeholder:-translate-y-20 placeholder:duration-[0.5s] md:col-span-2" placeholder="fullname" autocomplete="on" />
-                <input type="text" name="username" id="username" class="border-none w-full outline-none text-xl md:text-2xl px-6 py-3 text-center placeholder:font-poppy bg-off-brand placeholder-color font-poppy hover:placeholder:-translate-y-20 placeholder:duration-[0.5s]" placeholder="username" maxlength={15} minlength={4} autocomplete="on" />
-                <input type="email" name="email" id="email" class="border-none w-full outline-none text-xl md:text-2xl px-6 py-3 text-center placeholder:font-poppy bg-off-brand placeholder-color font-poppy hover:placeholder:-translate-y-20 placeholder:duration-[0.5s]" placeholder="email" autocomplete="on" />
-                <input type="password" name="password" id="password" class="border-none w-full outline-none text-xl md:text-2xl px-6 py-3 text-center placeholder:font-poppy bg-off-brand placeholder-color font-poppy hover:placeholder:-translate-y-20 placeholder:duration-[0.5s]" placeholder="password" autocomplete="on" />
-                <input type="password" name="confirm" id="confirm" class="border-none w-full outline-none text-xl md:text-2xl px-6 py-3 text-center placeholder:font-poppy bg-off-brand placeholder-color font-poppy hover:placeholder:-translate-y-20 placeholder:duration-[0.5s]" placeholder="confirm" autocomplete="on" />
+                <input type="text" name="fullname" id="fullname" class="border-none outline-none w-full text-xl md:text-2xl px-3 py-3 text-center placeholder:font-poppy bg-off-brand placeholder-color font-poppy hover:placeholder:-translate-y-20 placeholder:duration-[0.5s] md:col-span-2" placeholder="fullname" autocomplete="on" oninput="validateFullname()" />
+                <p class="w-full font-poppy text-red-500 text-2xl text-center" id="error-fullname"></p>              
+                <input type="text" name="username" id="username" class="border-none w-full outline-none text-xl md:text-2xl px-6 py-3 text-center placeholder:font-poppy bg-off-brand placeholder-color font-poppy hover:placeholder:-translate-y-20 placeholder:duration-[0.5s]" placeholder="username" maxlength={15} minlength={4} autocomplete="on" oninput="validateUsername()" />
+                <p class="w-full font-poppy text-red-500 text-2xl text-center" id="error-username"></p>
+                <input type="email" name="email" id="email" class="border-none w-full outline-none text-xl md:text-2xl px-6 py-3 text-center placeholder:font-poppy bg-off-brand placeholder-color font-poppy hover:placeholder:-translate-y-20 placeholder:duration-[0.5s]" placeholder="email" autocomplete="on" oninput="validateEmail()"/>
+                <p class="w-full font-poppy text-red-500 text-2xl text-center" id="error-email"></p>
+                <input type="password" name="password" id="password" class="border-none w-full outline-none text-xl md:text-2xl px-6 py-3 text-center placeholder:font-poppy bg-off-brand placeholder-color font-poppy hover:placeholder:-translate-y-20 placeholder:duration-[0.5s]" placeholder="password" autocomplete="on" oninput="validatePassword()" />
+                <p class="w-full font-poppy text-red-500 text-2xl text-center" id="error-password"></p>
+                <input type="password" name="confirm" id="confirm" class="border-none w-full outline-none text-xl md:text-2xl px-6 py-3 text-center placeholder:font-poppy bg-off-brand placeholder-color font-poppy hover:placeholder:-translate-y-20 placeholder:duration-[0.5s]" placeholder="confirm" autocomplete="on" oninput="validateConfirm()"/>
+                <p class="w-full font-poppy text-red-500 text-2xl text-center" id="error-confirm"></p>
                 <input type="submit" value="explore" name="submit" id="submit" class="py-[0.50rem] md:py-[0.70rem] w-44 md:col-span-2 text-white px-9 hover:cursor-pointer text-xl font-poppy rounded-md hover: duration-500" />
             </form>
             <div class='error-div'>
@@ -139,11 +206,6 @@
             }
         } else {
             $error_cred = true;
-        }
-        if ($errfullname) {
-            echo "<script>document.getElementById('error-name').innerHTML='Error in full name!';</script>";
-            echo "<script>document.getElementById('fullname').classList.add('error');</script>";
-            // echo "<script>alert('FULL NAME CANT HAVE NUMBERS');</script>";
         }
         if ($errusername) {
             echo "<script>document.getElementById('error-username').innerHTML='Username must contain only Alphabets!';</script>";
