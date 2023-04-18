30dbc8b511d934d982a5ef6bce9f38a45b42868e
devil-striker <darshaninsta360@gmail.com>
2023-04-18 15:44:50 +0530

    some changes

--- drops/view.php
+++ drops/view.php
@@ -16,7 +16,7 @@
 </head>
 
 <body>
-    <div class="bg-brand pb-16 bg-img bg-fixed w-full flex flex-col items-center">
+    <div class="bg-brand bg-img bg-fixed w-full flex flex-col items-center">
 
         <div class="flex flex-col items-center justify-center w-full">
 
@@ -33,7 +33,7 @@
                             <a href="../index.php" class="w-full pl-5 pt-3 h-20 z-20 text-3xl md:text-4xl font-colvet text-white absolute bg-gradient-to-b from-black">
                                 eatables.
                             </a>
-                            <video autoplay controls id="video-<?= $video['drop_id'] ?>" class="re playable-video " data-no-fullscreen="true" src="../drops/uploads/<?= $video['video_url'] ?>"></video>
+                            <video controls id="video-<?= $video['drop_id'] ?>" class="re playable-video " data-no-fullscreen="true" src="../drops/uploads/<?= $video['video_url'] ?>"></video>
                             <div class="w-full h-36 px-2 md:px-5 flex justify-between absolute z-50 bottom-0 font-poppy bg-gradient-to-t from-black text-white">
                                 <div class="flex flex-col w-3/4">
                                     <div class="flex items-center my-2 space-x-2">
@@ -43,14 +43,12 @@
                                     <p class="md:text-lg text-sm"> Omelette Cheese Burger | Hamburg Street Food Cafe</p>
                                 </div>
                                 <div class="flex flex-col items-center justify-evenly space-y-3">
-                                    <i class="fa-regular  fa-heart text-3xl cursor-pointer hover:text-red-500"></i>
+                                    <i class="fa-regular fa-heart text-3xl cursor-pointer hover:text-red-500"></i>
                                     <i class="fa-regular fa-bookmark text-3xl cursor-pointer"></i>
                                 </div>
                             </div>
                         </div>
-        </div>
-        <div class="flex flex-row items-center justify-center w-full">
-        <?php
+                    <?php
                     }
                 }
             } else {
@@ -59,33 +57,38 @@
                 $res = mysqli_query($con, $sql);
                 if (mysqli_num_rows($res) > 0) {
                     while ($video = mysqli_fetch_assoc($res)) {
-        ?>
-            <video controls="hidden" id="video-<?= $video['drop_id'] ?>" class="re playable-video h-[30rem]" data-no-fullscreen="true" src="drops/uploads/<?= $video['video_url'] ?>"></video>
-
-<?php
+                    ?>
+                        <div class="w-[420px] h-[780px] shadow-xl relative ">
+                            <h1 class="text-3xl md:text-4xl font-colvet text-white absolute ml-3">
+                                eatables.
+                            </h1>
+                            <video controls="hidden" id="video-<?= $video['drop_id'] ?>" class="re playable-video" data-no-fullscreen="true" src="drops/uploads/<?= $video['video_url'] ?>"></video>
+                        </div>
+            <?php
                     }
                 } else {
                     echo "<h1>Empty</h1>";
                 }
             }
-?>
-<?php
-if (!isset($_GET['review'])) {
-    if (isset($_GET['error'])) {
-?>
-        <p>
-            <?= $_GET['error'] ?>
-        </p>
-    <?php } ?>
-    <!-- <form action='upload.php' method="post" enctype="multipart/form-data" class="flex">
+            ?>
+            <?php
+            if (!isset($_GET['review'])) {
+                if (isset($_GET['error'])) {
+                    ?>
+                    <p>
+                        <?= $_GET['error'] ?>
+                    </p>
+                <?php } ?>
+                <!-- <form action='upload.php' method="post" enctype="multipart/form-data" class="flex">
                     <input type="file" name="my_video" class="hover:cursor-pointer font-poppy file:py-3 text-center file:border-0 file:px-6 bg-off-brand w-full">
                     <input type="submit" class="py-[0.50rem] md:py-[0.70rem] tracking-wider px-9 md:px-12 text-xl font-poppy rounded-md duration-500" name="submit" value="Upload">
                 </form> -->
-<?php
-}
-?>
+                <?php
+            }
+            ?>
         </div>
     </div>
+
 </body>
 
 </html>
