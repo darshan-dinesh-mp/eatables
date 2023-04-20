3b815c4127ed711f8fceb7460f7e54f75b37ce46
Delbin George <imdelbingeorge@gmail.com>
2023-04-20 23:14:15 +0530

    admin page styled

--- admin/manage_restaurant.php
+++ admin/manage_restaurant.php
@@ -1,18 +1,15 @@
 <?php
 include "dbconnect.php";
-$sql="select * from hotel";
-$res=$con->query($sql);
-if($res->num_rows>0)
-{
-    echo "<table border=1>";
-	while($row=$res->fetch_assoc())
-	{
-		echo "<tr><th>$row[hotel_name]</th>";
-        echo "<td><a href='admin/edit.php?hname=$row[hotel_name]'>EDIT</a></td>";
-        echo "<td><a href='admin/delete_hotel.php?hname=$row[hotel_name]'>DELETE</a></td>";
+$sql = "select * from hotel";
+$res = $con->query($sql);
+if ($res->num_rows > 0) {
+    echo "<table class='w-full text-center'>";
+    echo "<tr class='w-full bg-gray-300 h-14 text-xl'><th class='w-1/3'>Hotel Name</th><th class='w-1/3'>Edit</th><th>Delete</th></tr>";
+    while ($row = $res->fetch_assoc()) {
+        echo "<tr class='h-14 odd:bg-gray-300'><td class='w-2/4'>$row[hotel_name]</td>";
+        echo "<td class='w-1/4'><a href='admin/edit.php?hname=$row[hotel_name]'><i class='fa-solid fa-pen-to-square text-lg hover:text-[#f9bb21]'></i></a></td>";
+        echo "<td class='w-1/4'><a href='admin/delete_hotel.php?hname=$row[hotel_name]'><i class='fa-solid fa-trash text-lg hover:text-[#f9bb21]'></i></a></td>";
         echo "</tr>";
-		echo "<br>";
-	}
+    }
     echo "</table>";
 }
-?>
