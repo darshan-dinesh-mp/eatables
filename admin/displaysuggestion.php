068547e61ef10dc935301e37253c0923bc0ec3d7
Delbin George <imdelbingeorge@gmail.com>
2023-04-26 21:01:59 +0530

    askldjf;alsjdfljS

--- admin/displaysuggestion.php
+++ admin/displaysuggestion.php
@@ -4,7 +4,11 @@ $sql = "select * from suggestion";
 $res = $con->query($sql);
 if ($res->num_rows > 0) {
     echo "<table class='w-full text-center'>";
-    echo "<tr class='w-full bg-gray-300 h-14 text-xl'><th class='w-1/3'>Hotel Name</th><th class='w-1/3'>Suggested Hotel Details</th></tr>";
+    echo "<tr class='w-full bg-gray-300 h-14 text-xl'>
+                <th class='w-1/3'>Hotel Name</th>
+                <th class='w-1/3'>Suggested Hotel Details</th>
+                <th class='w-1/3'>Action</th>
+                </tr>";
     while ($row = $res->fetch_assoc()) {
         echo "<tr class='h-14 odd:bg-gray-300'><td class='w-2/4'>$row[uname]</td>";
         echo "<td class='w-1/4'>$row[hotel_name]<br>$row[hotel_loc]<br>$row[ratings]<br>$row[links]<br>$row[desc]</td>";
@@ -13,4 +17,3 @@ if ($res->num_rows > 0) {
     }
     echo "</table>";
 }
-?>
