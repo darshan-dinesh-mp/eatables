f45ac8bc1f3f9715ebe5c87f752407bd5d10937e
Darshan Dinesh <darshandineshmp@gmail.com>
2024-07-24 19:25:23 +0530

    more changes in filter_result.php

--- BOT/filtered_results.php
+++ BOT/filtered_results.php
@@ -16,6 +16,11 @@ include "../dbconnect.php";
 
 <body>
   <div class="bg-brand bg-img bg-fixed min-h-screen flex flex-col items-center p-4 md:px-16">
+    <div class="flex items-center w-full justify-between margin-one z-10">
+      <a href="../index.php" class="text-3xl md:text-4xl font-colvet">
+        eatables.
+      </a>
+    </div>
     <div class="flex items-center justify-evenly absolute top-[-5px] h-[100px] w-full ml-[-312px]">
       <img class='mt-32 w-[300px]' src="../media/images/funny-3d-illustration-cartoon-teenage-girl.png" alt="img" />
       <h1 class='font-poppy font-bold text-2xl'>Personalized Result</h1>
@@ -30,7 +35,7 @@ include "../dbconnect.php";
       $minPrice = 101;
       $maxPrice = 500;
     } elseif ($priceRange == 'high') {
-      $minPrice = 501; 
+      $minPrice = 501;
       $maxPrice = 999;
     }
     $sql1 = "SELECT count(*) as total from item where item_price >= $minPrice AND item_price <= $maxPrice";
@@ -57,22 +62,25 @@ include "../dbconnect.php";
         while (($row = $res->fetch_assoc())) {
 
           $img_links = $row['item_img'];
-      ?>
-          <a class="text-white hover:scale-[1.01] rounded-lg shadow-lg px-4 py-24 hover:shadow-xl transition-all font-poppy font-semibold text-center duration-500" style="background-image:linear-gradient(to top, rgba(0, 0, 0, 0.916), rgba(0, 0, 0, 0.155)), url('<?php echo $img_links; ?>'); background-size:cover;" href="../itempage.php?item_id=<?php echo $row['item_id']; ?>">
+          ?>
+          <a class="text-white hover:scale-[1.01] rounded-lg shadow-lg px-4 py-24 hover:shadow-xl transition-all font-poppy font-semibold text-center duration-500"
+            style="background-image:linear-gradient(to top, rgba(0, 0, 0, 0.916), rgba(0, 0, 0, 0.155)), url('<?php echo $img_links; ?>'); background-size:cover;"
+            href="../itempage.php?item_id=<?php echo $row['item_id']; ?>">
             <p class="text-white-700 text-2xl"><?php echo $row['item_name']; ?></p>
             <p class="text-white-700 text-2xl">₹<?php echo $row['item_price']; ?></p>
           </a>
-      <?php
+          <?php
         }
       } ?>
     </div>
     <div class="pagination w-3/4 overflow-x-scroll h-32 flex items-center justify-center space-x-3 pt-8">
-      <?php for ($i = 1; $i <= $total_pages; $i++) : ?>
-        <a class="font-poppy" href="?price=<?php echo $priceRange  ?>&page=<?php echo $i ?>" class="<?php if ($i == $page) echo 'active' ?>"><?php if ($_GET['page'] == $i) {
-                                                                                                                                                echo "<span class='font-bold text-lg bg-black/30 px-4 py-2 rounded-lg'>$i</span>";
-                                                                                                                                              } else {
-                                                                                                                                                echo "<span class='font text-lg bold hover:bg-black/30 px-4 py-2 rounded-lg'>$i</span>";
-                                                                                                                                              } ?></a>
+      <?php for ($i = 1; $i <= $total_pages; $i++): ?>
+        <a class="font-poppy" href="?price=<?php echo $priceRange ?>&page=<?php echo $i ?>" class="<?php if ($i == $page)
+                echo 'active' ?>"><?php if ($_GET['page'] == $i) {
+                echo "<span class='font-bold text-lg bg-black/30 px-4 py-2 rounded-lg'>$i</span>";
+              } else {
+                echo "<span class='font text-lg bold hover:bg-black/30 px-4 py-2 rounded-lg'>$i</span>";
+              } ?></a>
       <?php endfor; ?>
     </div>
   </div>
